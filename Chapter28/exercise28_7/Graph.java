88a67c8314ee2fb400a3a4f1690ba2c909f9f9ab
travelingtechman <53628990+travelingtechman@users.noreply.github.com>
2023-01-26 15:27:29 -0700

    Add files via upload

---
+++ Chapter28/exercise28_7/Graph.java
@@ -0,0 +1,43 @@
+package exercise28_7;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public interface Graph<V> {
+	  /** Return the number of vertices in the graph */
+	  public int getSize();
+
+	  /** Return the vertices in the graph */
+	  public java.util.List<V> getVertices();
+
+	  /** Return the object for the specified vertex index */
+	  public V getVertex(int index);
+
+	  /** Return the index for the specified vertex object */
+	  public int getIndex(V v);
+
+	  /** Return the neighbors of vertex with the specified index */
+	  public java.util.List<Integer> getNeighbors(int index);
+
+	  /** Return the degree for a specified vertex */
+	  public int getDegree(int v);
+
+	  /** Print the edges */
+	  public void printEdges();
+
+	  /** Clear the graph */
+	  public void clear();
+
+	  /** Add a vertex to the graph */  
+	  public boolean addVertex(V vertex);
+
+	  /** Add an edge to the graph */  
+	  public boolean addEdge(int u, int v);
+
+	  /** Obtain a depth-first search tree */
+	  public UnweightedGraph<V>.SearchTree dfs(int v);
+
+	  /** Obtain a breadth-first search tree */
+	  public UnweightedGraph<V>.SearchTree bfs(int v);
+	}
---
+++ Chapter28/exersice28_03/Edge.java
@@ -0,0 +1,14 @@
+package exersice28_03;
+public class Edge {
+	int u;
+	int v;
+	
+	public Edge(int u, int v) {
+		this.u = u;
+		this.v = v;
+	}
+	
+	public boolean equals(Object o) {
+		return (u == ((Edge)o).u && v == ((Edge)o).v);
+	}
+}
